24cdc4b00fc2e01e6d29a6f9ad5cd7f3e6036c35
ismailyurdakal <ismail94@yahoo.com>
2021-06-30 18:23:34 +0000

    users added

--- Class1/provider.tf
+++ Class1/provider.tf
@@ -4,4 +4,25 @@ provider "aws"{
 
 resource "aws_iam_user" "tim" {
     name = "tim" 
+path = "/system/"
+tags = {
+    Team = "DevOps"
+  }
+}
+resource "aws_iam_user" "bob" {
+    name = "bob" 
+path = "/system/"
+tags = {
+    Team = "DevOps"
+  }
+
+}
+
+resource "aws_iam_user" "john" {
+    name = "john" 
+path = "/system/"
+tags = {
+    Team = "DevOps"
+  }
+
 }
