de225f5fb6ea8a637dbf90538425319b6f6abaca
ismailyurtdakal <ismail94@yahoo.com>
2021-07-05 22:16:20 +0000

    Class2 and Class3 added

---
+++ Class3/newami.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.20210617.0-x86_64-gp2*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
