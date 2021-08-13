3e9dc6a4ba3cdb53a75dba2bcd149ff13c1032c9
ismailyurtdakal <ismail94@yahoo.com>
2021-08-13 09:54:48 +0000

    var updated

--- Class4/key.tf
+++ Class4/key.tf
@@ -1,15 +1,7 @@
 resource "aws_key_pair" "automation" {
-  key_name   = "automation"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    Name = "DevOps"
-  }
+  key_name   = var.key_name
+  public_key = file(var.public_key)
 }
 
-resource "aws_key_pair" "automation2" {
-  key_name   = "automation2"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    Name = "DevOps"
-  }
-}
+
+
--- Class4/terraform.auto.tfvars
+++ Class4/terraform.auto.tfvars
@@ -1,1 +1,3 @@
-region="us-east-1"
+region     = "us-east-1"
+key_name   = "automation"
+public_key = "~/.ssh/id_rsa.pub"
