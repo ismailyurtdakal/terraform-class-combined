96cb13bd9c11285830024a73acdbb9474391e4de
ismailyurtdakal <ismail94@yahoo.com>
2021-08-14 11:14:21 +0000

    encrpytion added

--- Class5/s3.tf
+++ Class5/s3.tf
@@ -1,9 +1,23 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "bucket2" {
   bucket_prefix = "s3_class5_bucket-"
   acl           = "private"
 
   versioning {
     enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
 
   tags = {
